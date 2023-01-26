941e188d29fbc8721d527be825292d331be1a6ed
z0ld1ck <91350635+z0ld1ck@users.noreply.github.com>
2023-01-26 17:34:45 +0600

    initial commit

--- user/models.py
+++ user/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import service
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+STATUS_CHOICES = [
+    ('1', 'CLIENT'),
+    ('2', 'ORG')
+]
+
+
+class User(models.Model):
+    userId = models.CharField(primary_key=True)
+    name = models.CharField()
+    surname = models.CharField()
+    password = models.CharField()
+    status = models.CharField(choices=STATUS_CHOICES, default='1')
+    avatar = models.ImageField(name='Avatar')
+    city = models.CharField()
+    # zapisi=models.CharField()
+
+
